be66b6dc99107831bba55b10654971f40ac5a9cb
amauri souza Programador <amauridesouzaferreira@gmail.com>
2023-01-28 12:00:39 -0300

    Creação do Render index e Vue Components

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        return 'project';
+        return Inertia::render('Projects/Index');
     }
 }
