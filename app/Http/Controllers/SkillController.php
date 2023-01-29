a01e80146c679dc397dc2d2e73bc96858f2ed7e6
amauri souza Programador <amauridesouzaferreira@gmail.com>
2023-01-29 15:14:44 -0300

    Criada a Store new Skill

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class SkillController extends Controller
 {
@@ -15,5 +17,26 @@ class SkillController extends Controller
     public function create()
     {
         return Inertia::render('Skills/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'image'=> ['required'],
+            'name' => ['required','min:3'],
+
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('skills');
+            Skill::create([
+                'name' => $request->name,
+                'image' => $image
+            ]);
+
+            return Redirect::route('skills.index');
+        }
+
+        return Redirect::back();
     }
 }
