a6ac4b0cd18933dd1518501cffe469997b77c60c
amauri souza Programador <amauridesouzaferreira@gmail.com>
2023-02-06 15:00:33 -0300

    Create Frontend Layout Navibar

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,14 +1,9 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
-
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-});
+import Frontend from '@/Layouts/Frontend.vue';
 </script>
 
 <template>
     <Head title="Bem vindo a o meu" />
+    <Frontend></Frontend>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,37 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require("@tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         fontFamily: {
             primary: "Playfair Display",
-            body: "Work Sans"
+            body: "work Sans",
         },
         container: {
             padding: {
                 DEFAULT: "1rem",
-                lg: "3rem"
+                lg: "3rem",
             },
         },
         extend: {
             colors: {
-                "ligth-primary": "#FAF1E6",
-                "ligth-secondary": "#FDFAF6",
-                "ligth-primary": "#A9DED2",
-                "ligth-primary": "#54BAB9",
+                "light-primary": "#FAF1E6",
+                "light-secondary": "#FDFAF6",
+                "light-tail-100": "#A9DED2",
+                "light-tail-500": "#54BAB9",
                 "dark-primary": "#050402",
                 "dark-secondary": "#1C1D24",
-                "dark-navy": "#07567D",
-                "dark-navy": "#292D42",
-                accept: {
+                "dark-navy-100": "#07567D",
+                "dark-navy-500": "#292D42",
+                accent: {
                     DEFAULT: "#ac6b34",
                     hover: "#925a2b",
                 },
@@ -41,9 +41,8 @@ module.exports = {
                 green: colors.green,
                 red: colors.red,
             },
-        }
-        
+        },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
