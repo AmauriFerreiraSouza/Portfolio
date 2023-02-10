1b47634340c10d1123781f39c98dda6b9151f7e1
amauri souza Programador <amauridesouzaferreira@gmail.com>
2023-02-10 00:15:11 -0300

    Create Hero Section Portfólio

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import FrontendLayout from '@/Layouts/Frontend.vue';
+import Hero from '@/Components/Frontend/Hero.vue';
 </script>
 
 <template>
     <Head title="Bem vindo a o meu" />
     <FrontendLayout>
         <!-- Hero primary -->
+        <Hero/>
         <!-- Promote tail--500 -->
         <!-- About secondary -->
         <!-- Skills tail-100 -->
